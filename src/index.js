4cb44171aedd3d317af2ae7802b9eb2b197761f1
mchuongUIT1852 <chuonghuynh240@gmail.com>
2021-04-30 15:13:19 +0700

    Create app/controllers & routers folder

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ const morgan = require('morgan')
 const handlebars  = require('express-handlebars')
 const app = express()
 const port = 3000
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname, 'public')))
-
 app.use(morgan('combined'))
+
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 app.engine('hbs', handlebars({
   extname: '.hbs'
@@ -16,9 +21,8 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home') 
-})
+// Router init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
